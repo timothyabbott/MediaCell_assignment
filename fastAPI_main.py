5170a1a87d04943f045b63b8b4f435e365966aea
timabbott <20151421+timothyabbott@users.noreply.github.com>
2024-05-06 21:20:17 +0100

    add cors, rename code to id amd action to codeword

--- fastAPI_main.py
+++ fastAPI_main.py
@@ -2,8 +2,20 @@ from typing import  Annotated
 
 import uvicorn
 from fastapi import FastAPI, Path, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 data = \
     {
@@ -18,18 +30,18 @@ data = \
     }
 
 
-@app.get('/actions/codeword/{code}', status_code=status.HTTP_200_OK)
-def action_from_code(code: Annotated[int, Path(title="The ID of the action")]):
+@app.get('/actions/codeword/{id}', status_code=status.HTTP_200_OK)
+def action_from_code(id: Annotated[int, Path(title="The ID of the action")]):
     for item in data['actions']:
-        if code in item.values():
+        if id in item.values():
             return item.get('codeword')
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Action not found")
 
 
-@app.get('/actions/code/{action}', status_code=status.HTTP_200_OK)
-def code_from_action(action: Annotated[str, Path(title="The title of the action")]):
+@app.get('/actions/id/{codeword}', status_code=status.HTTP_200_OK)
+def code_from_action(codeword: Annotated[str, Path(title="The title of the action")]):
     for item in data['actions']:
-        if action in item.values():
+        if codeword in item.values():
             return item.get('id')
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Action not found")
 
